803fd714c2a9d745f4a036ed57d79c766f9976da
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2020-06-14 19:54:10 -0300

    Teste de Commit

--- ExportadorContatos/Program.cs
+++ ExportadorContatos/Program.cs
@@ -18,6 +18,7 @@ namespace ExportadorContatos
             //Application.Run(new ExportadorContatosGoogle.FormGerenciadorContatosVCardV4());
             //Application.Run(new ExportadorContatosGoogle.Form1());
             Application.Run(new ExportadorContatosGoogle.FormGerenciadorContatosVCardV3());
+            //teste
         }
     }
 }
